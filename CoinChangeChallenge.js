7d5c8bfe64ffc8fab9f0149b8cc509bb1451433a
Chris Wargo <Crwargo0783@gmail.com>
2020-04-29 19:08:57 -0400

    refactored so you can use decimal values. More realistic

--- CoinChangeChallenge.js
+++ CoinChangeChallenge.js
@@ -1,9 +1,17 @@
+
+//Given the price of an item and the coins you will use to pay for said item, calculate the most efficient combination of change you would get back from your purchase. 
+//Example: CoinChange(1.00, [.25, .25, .25, .25, .25, .05]); SHOULD RETURN [`Pennies: 0, Nickels: 1, Dimes: 0, Quarters: 1`] OR Pennies: 0, Nickels: 1, Dimes: 0, Quarters: 1
+
+
 const CoinChange = (price, coins) => {
+  //multiply price and coins by 100 so you can use Math.floor
+  let newPrice = price * 100
+  let newCoins = coins.map(function (x) { return x * 100; });
   //reduce coins array down to a single value
   const reducer = (a, b) => a + b;
-  let coinValue = coins.reduce(reducer);
+  let coinValue = newCoins.reduce(reducer);
   //subtract coin value by price by price
-  let cents = coinValue - price;
+  let cents = coinValue - newPrice;
   //find out how many of each denomination can go into the cents value. Subtract the value as you switch denominations.
   let quarters = Math.floor(cents / 25);
   cents = cents - 25 * quarters;
@@ -13,14 +21,18 @@ const CoinChange = (price, coins) => {
   cents = cents - 5 * nickels;
   let pennies = cents;
   //if you want to get the value back as an array, we can push the values into an empty array and then console.log it.
+  //if you want decimal values, divide 
   let change = [];
   change.push(
-    `Pennies ${pennies}, Nickels: ${nickels}, Dimes: ${dimes}, Quarters: ${quarters}`
+    `Pennies: ${pennies}, Nickels: ${nickels}, Dimes: ${dimes}, Quarters: ${quarters}`
   );
   console.log(
     `Pennies: ${pennies}, Nickels: ${nickels}, Dimes: ${dimes}, Quarters: ${quarters}`
   );
   console.log(change);
+
 };
 
-CoinChange(100, [25, 25, 25, 25, 25, 5, 5, 5, 10, 1, 1, 5, 10, 10, 5, 10]);
+CoinChange(1.00, [.25, .25, .25, .25, .25, .05]);
+
+
